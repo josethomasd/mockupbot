3b794ef672b5739d952487312514ac9a58a63d46
josethomasd <josethomasd@gmail.com>
2017-08-24 00:38:52 +0530

    dB

--- app.py
+++ app.py
@@ -71,10 +71,41 @@ def webhook():
                 # the facebook ID of the person sending you the message 
                 message_text = data["entry"][0]["messaging"][0]["message"]["text"]  # the message's text
                 if(user.countval==0):
-                    message_data = "Yo"
+                    message_data = "Hi there!  Returns are accepted when items are in their original condition: unwashed and unworn. Items not in this condition will not be accepted. You have up to 45 days to return items purchased. Items may be returned to your local store, Returns and exchanges are free when you use our prepaid return shipping label. Was that helpful?"
+                elif(user.countval==1):
+                    message_data = "OK, no problem. I need to get some information from you. What is your email?"
+                elif(user.countval==2):
+                    message_data = "Thank you! Is your name Jessica Stanfield?"
+                elif(user.countval==3):
+                    message_data = "Jessica, I see here that the last order you placed was Order #AA56789. Do you want to return the entire order, or just part of it?"
+                elif(user.countval==4):
+                    message_data = "I see that you ordered 2 pairs of shoes. Do you want to return both pairs?"
+                elif(user.countval==5):
+                    message_data = "So you want to return the Salvatore Ferragamo Black Patent Flats with Grosgrain Bow in Size 8. Is that correct?"
+                elif(user.countval==6):
+                    message_data = "Can you tell me why? Simply choose the reason that most closely matches your reasons for returning this product. For multiple reasons, just type the numbers separated by commas, like so: 1, 3"
+                elif(user.countval==7):
+                    message_data = "Would you be interested in exchanging the original pair for a larger size? I see that we have a Size 8.5 and a Size 9 in stock in the same color."
+                elif(user.countval==8):
+                    message_data = "OK, I just want to confirm that you want to exchange the Salvatore Ferragamo Black Patent Flats with Grosgrain Bow in Size 8 for Size 8.5."
+                elif(user.countval==9):
+                    message_data = "Do you still have the shipping label that was sent to you with your order?"
+                elif(user.countval==10):
+                    message_data = "Don't worry, I will email a new shipping label to myemail@gmail.com for the exchange. Please send us back the Salvatore Ferragamo Black Patent Flats with Grosgrain Bow Size 8 shoes in this box with the shipping label I am emailing you now."
+                elif(user.countval==11):
+                    message_data = "Do you want me to send the Salvatore Ferragamo Black Patent Flats with Grosgrain Bow in Size 8.5 to the same address as the original order? Type ‘yes' if you do, ‘no' if you want me to send them to a new address."
+                elif(user.countval==12):
+                    message_data = "OK, can you confirm the address I have is correct?\n\nJessica Stanfield\n123 Pine Heights Blvd.\nNiceville, CA 12345"
+                elif(user.countval==13):
+                    message_data = " Thank you. Once the package carrier scans the return label on your original order, we will automatically send you your exchange order Salvatore Ferragamo Black Patent Flats with Grosgrain Bow Size 8.5."
+                elif(user.countval==14):
+                    message_data = "I'm happy to help! Is there anything else I can do for you? Just type ‘yes' and what I can help you with, or ‘bye'. "
                 else:
-                    message_data = "Yoyo"
-                send_message(sender_id, message_data)
+                    message_data = "Have a great day, Jessica!  :)"
+                if(user.countval==6):
+                    send_button(sender_id,message_data)
+                else:
+                    send_message(sender_id, message_data)
                 user.countval = user.countval+1
                 db.session.commit()
                     #send_button(sender_id, message_data)
